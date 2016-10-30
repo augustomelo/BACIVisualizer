5d372969fe158e5fcaec48114d1ac95ffd69a66b
Augusto Melo <augustomelo92@gmail.com>
2016-10-30 18:27:04 -0200

    Added list questionnaire.
    This is how the list questionnaire should look, later it will be
    transform into a component.

--- src/components/ListQuestionnaire.vue
+++ src/components/ListQuestionnaire.vue
@@ -3,6 +3,20 @@
         <span class="card-title">Questionarios</span>
         <hr>
         <br />
+        <div class="row my-row">
+            <div class="input-field col s3">
+                <select id="entries" class="vai">
+                    <option value="1">10</option>
+                    <option selected value="2">50</option>
+                    <option value="3">100</option>
+                </select>
+                <label for="entries">Escolha quantas entradas</label>
+            </div>
+            <div class="input-field col s9">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Pesquisar</label>
+            </div>
+        </div>
         <table class="striped">
             <thead>
                 <tr>
@@ -13,15 +27,96 @@
             </thead>
 
             <tbody>
-                <!--Add Vue code-->
+                <tr @dblclick="showFullData(1)">
+                    <td>Eu</td>
+                    <td>Eu</td>
+                    <td>Eu</td>
+                </tr>
+                <tr @dblclick="showFullData(2)">
+                    <td>Vc</td>
+                    <td>Vc</td>
+                    <td>Vc</td>
+                </tr>
+                <tr @dblclick="showFullData(3)">
+                    <td>Ele</td>
+                    <td>Ele</td>
+                    <td>Ele</td>
+                </tr>
+                <tr @dblclick="showFullData(4)">
+                    <td>Tu</td>
+                    <td>Tu</td>
+                    <td>Tu</td>
+                <tr @dblclick="showFullData(5)">
+                    <td>Eu</td>
+                    <td>Eu</td>
+                    <td>Eu</td>
+                </tr>
+                </tr>
             </tbody>
         </table>
+
+        <div class="row">
+            <div class="col s7 left-align my-label-info">
+                <h6>
+                    Mostrando 1 of 10 of 19 entradas
+                </h6>
+            </div>
+            <div class="col s5 right-align">
+                <ul class="pagination">
+                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                    <li class="active"><a href="#!">1</a></li>
+                    <li class="waves-effect"><a href="#!">2</a></li>
+                    <li class="waves-effect"><a href="#!">3</a></li>
+                    <li class="waves-effect"><a href="#!">4</a></li>
+                    <li class="waves-effect"><a href="#!">5</a></li>
+                    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
-</template>>
+</template>
 
 <script>
+    export default {
+        mounted() {
+            $('select').material_select();
+        },
+        methods: {
+            showFullData: function(number) {
+                console.log(number);
+            }
+        }
+    };
 </script>
 
+<style lang="sass">
+.my-row
+    margin-bottom: 0
 
-<style lang="sass" scoped>
+table.striped>tbody>tr:nth-child(odd)
+    background-color: #c5e1a5
+
+.input-field input[type=text]:focus + label
+     color: #9ccc65;
+
+.input-field input[type=text]:focus
+    border-bottom: 1px solid #9ccc65
+    box-shadow: 0 1px 0 0 #9ccc65
+
+/* It is not scoped because of this one */
+.dropdown-content
+    background-color: #f1f8e9
+
+.dropdown-content>li>span
+    color: #000
+
+.dropdown-content li:houver
+    color: #000
+/*End*/
+
+.pagination li.active
+    background: #9ccc65
+
+.my-label-info
+    padding-top: 15px
 </style>
