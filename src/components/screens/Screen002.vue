615e68a932c805b02008b905779e17cf5cc0d1d3
Augusto Melo <augustomelo92@gmail.com>
2016-12-14 12:18:41 -0200

    Bind the model and the data.

--- src/components/screens/Screen002.vue
+++ src/components/screens/Screen002.vue
@@ -8,25 +8,25 @@
         <div class="row">
             <div class="row">
                 <div class="input-field col s6">
-                    <input disabled value="" type="text" class="datepicker"> 
+                    <input disabled v-model="data.Tela2.dataAplicacao" type="text">
                     <label for="disabled">Data de Aplição</label>
                 </div>
                 <div class="input-field col s6">
-                    <input disabled value="" type="text" class="validate">
+                    <input disabled v-model="data.Tela2.nomeAvaliado" type="text" class="validate">
                     <label for="disabled">Nome do Avaliado</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s4">
-                    <input disabled type="text" class="datepicker">
+                    <input disabled v-model="data.Tela2.dataNascimento" type="text">
                     <label for="disabled">Data de Nascimento</label>
                 </div>
                 <div class="input-field col s4">
-                    <input disabled value="" type="text" class="validate">
+                    <input disabled v-model="data.Tela2.idade" type="text" class="validate">
                     <label for="disabled">Idade</label>
                 </div>
                 <div class="input-field col s4">
-                    <select disabled>
+                    <select disabled v-model="data.Tela2.lateralidade">
                         <option value="empty"></option>
                         <option value="destro">Destro</option>
                         <option value="canhoto">Canhoto</option>
@@ -36,57 +36,57 @@
             </div>
 
             <div class="row">
-                <div class="input-field col s3">
-                    <select disabled>
+                <div class="input-field col s4">
+                    <select disabled v-model="data.Tela2.escolaridade">
                         <option value="empty"></option>
-                        <option value="nunca">Analfabeto/nunca frequentou escola</option>
-                        <option value="0a3">0 á 3 anos de escolarizçao</option>
-                        <option value="4a8">4 á 8 anos de escolarizçao</option>
-                        <option value="mais8">mais de 8 anos de escolarizçao</option>
+                        <option value="analfabeto">Analfabeto/nunca frequentou escola</option>
+                        <option value="inicio">0 á 3 anos de escolarizçao</option>
+                        <option value="medio">4 á 8 anos de escolarizçao</option>
+                        <option value="superior">mais de 8 anos de escolarizçao</option>
                     </select>
                     <label>Escolaridade</label>
                 </div>
-                <div class="input-field col s3">
-                    <input disabled value="" type="text" class="validate">
+                <div class="input-field col s4">
+                    <input disabled v-model="data.Tela2.profissao" type="text" class="validate">
                     <label for="disabled">Profissão</label>
                 </div>
-                <div class="input-field col s3">
-                    <select disabled>
+                <div class="input-field col s4">
+                    <select disabled v-model="data.Tela2.trabalhoAtual">
                         <option value="empty"></option>
-                        <option value="naoAposentado">Não, porque está aposentado</option>
-                        <option value="naoDesempregado">Não porque está desempregado/licenciado</option>
-                        <option value="simTrabalha">Sim, está trabalhando</option>
+                        <option value="aposentado">Não, porque está aposentado</option>
+                        <option value="desempregado">Não porque está desempregado/licenciado</option>
+                        <option value="trabalhando">Sim, está trabalhando</option>
                     </select>
                     <label>Trabalho Atual</label>
-                </div>
-                <div class="input-field col s3">
-                    <input disabled value="" type="text" class="validate">
-                    <label for="disabled">Natural de que cidade/estado</label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="input-field col s4">
-                    <select disabled>
+                <div class="input-field col s3">
+                    <input disabled v-model="data.Tela2.naturalidade" type="text" class="validate">
+                    <label for="disabled">Natural de que cidade/estado</label>
+                </div>
+                <div class="input-field col s3">
+                    <select disabled  v-model="data.Tela2.sexo">
                         <option value="empty"></option>
                         <option value="masculino">Masculino</option>
                         <option value="feminino">Feminino</option>
                     </select>
                     <label>Sexo</label>
                 </div>
-                <div class="input-field col s4">
-                    <select disabled>
+                <div class="input-field col s3">
+                    <select disabled v-model="data.Tela2.limitacaoCorporal">
                         <option value="empty"></option>
-                        <option value="naoLimitacao">Não</option>
-                        <option value="simLimitacao">Sim</option>
+                        <option value="naoCorporal">Não</option>
+                        <option value="simCorporal">Sim</option>
                     </select>
                     <label>Possui limitação corporal visível?</label>
                 </div>
-                <div class="input-field col s4">
-                    <select disabled>
+                <div class="input-field col s3">
+                    <select disabled v-model="data.Tela2.problemaVisual">
                         <option value="empty"></option>
-                        <option value="naoOculos">Não</option>
-                        <option value="simOculos">Sim</option>
+                        <option value="naoVisivel">Não</option>
+                        <option value="simVisivel">Sim</option>
                     </select>
                     <label for="disabled">Possui probelmas visual ou usa óculos?</label>
                 </div>
@@ -108,6 +108,7 @@
             $('.datepicker').pickadate({
                 format: 'dd-mm-yyyy'
             });
+            Materialize.updateTextFields()
         }
     };
 </script>
