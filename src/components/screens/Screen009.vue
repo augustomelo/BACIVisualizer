52dd8c7dcfa214be23862928cb59526e3f11bf27
Augusto Melo <augustomelo92@gmail.com>
2016-12-14 22:06:16 -0200

    Bind the model and the data screen 9

--- src/components/screens/Screen009.vue
+++ src/components/screens/Screen009.vue
@@ -8,26 +8,26 @@
 
         <div class="row">
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex1">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você está satisfeito com a sua vida?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex2">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você deixou muitos de sues interesses e atividades?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex3">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você sente que sua vida está vazia?</label>
             </div>
@@ -35,26 +35,26 @@
 
         <div class="row">
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex4">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se aborrece com frequencia?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex5">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente de bom humor a maior parte do tempo ?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex6">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você tem medo que algum mal vá le acontecer?</label>
             </div>
@@ -62,26 +62,26 @@
 
         <div class="row">
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex7">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente feliz a maior parte do tempo?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex8">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente que a sua sitação não tem saída?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex9">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você prefere fica em casa do que sair e fazer coisas nova?</label>
             </div>
@@ -89,26 +89,26 @@
 
         <div class="row">
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex10">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente com mais problemas de memória do que a maioria?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex11">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você acha maravilhoso estar vivo?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex12">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente um inútil nas atuais circustâncias?</label>
             </div>
@@ -116,26 +116,26 @@
 
         <div class="row">
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex13">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você se sente cheio de energia?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex14">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você acha que sua sitação é sem esperanças?</label>
             </div>
             <div class="input-field col s4">
-                <select disabled>
+                <select disabled v-model="data.Tela9.ex15">
                     <option value="empty"></option>
-                    <option value="nao">Não</option>
-                    <option value="simComAjuda">Sim</option>
+                    <option value="">Não</option>
+                    <option value="on">Sim</option>
                 </select>
                 <label for="disabled">Você sente que a maioria das pessoas está melhor que você?</label>
             </div>
@@ -146,9 +146,15 @@
 <script>
     export default {
         name: 'Screen009',
+        props: {
+            data: {
+                type: Object,
+            },
+        },
         mounted: function() {
             $('select').material_select();
-        }
+            Materialize.updateTextFields();
+        },
     };
 </script>
 
